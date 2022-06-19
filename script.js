871ec80bda692bcbd945c3f4286720a7862e1477
solphilos <trichosirius@gmail.com>
2022-06-18 21:10:44 -0400

    created module for gameplay

--- script.js
+++ script.js
@@ -1,53 +1,74 @@
-const gameBoard = (function() {
-    const gridArray = [];
 
+// gameboard module
+const gameBoard = (function() {
+         
+         // populates an array with divs selected thru hover event. Marks chosen square with an X or O.
+         chooseX = () => {
+            let chosen = document.querySelectorAll(':hover')
+            let gameboard = Array.from(chosen);
+            let gridSquare = gameboard.pop()
+            let text = gridSquare.textContent;
+            gridSquare.innerHTML = "X"
+            // prevents player choosing spot already taken
+            if (text === 'O') {
+                gridSquare.innerHTML = 'O';
+            }          
+         }
+        
+         chooseO = () => {
+            let chosen = document.querySelectorAll(':hover')
+            let gameboard = Array.from(chosen);
+            let gridSquare = gameboard.pop()
+            let text = gridSquare.textContent;
+            gridSquare.innerHTML = "O"
+            if (text === 'X') {
+                gridSquare.innerHTML = 'X';
+            } 
+         }
+        
+        
+        
+        removeListener = (funct) => {
+             document.querySelector('.grid_container').removeEventListener('click', funct)
+         }
+
+         return {
+             // creates buttons for choosing X or O gamepieces. 
+            addListeners: function() {
+                let choice;
+            
+                  document.querySelector('.choiceX').addEventListener('click', function() {
+                       choice = 'x';
+                       if (choice === 'x') {
+                          document.querySelector('.grid_container').addEventListener('click', chooseX);
+                         }
+                         removeListener(chooseO)
+                     })
+            
+                  document.querySelector('.choiceO').addEventListener('click', function() {
+                       choice = 'o';
+                       if (choice === 'o') {
+                          document.querySelector('.grid_container').addEventListener('click', chooseO);
+                         }
+                         removeListener(chooseX)
+                     })
+                    }
+         }
 })();
 
+//end of game/whos the winner? module
+const gameEnd = (() => {
+    
+})();
+
+gameBoard.addListeners();
 
 
 
 
-chooseX = () => {
-    let chosen = document.querySelectorAll(':hover')
-    let array = Array.from(chosen);
-    let idName = array.pop()
-    idName.innerHTML = "X"
- }
-
- chooseO = () => {
-    let chosen = document.querySelectorAll(':hover')
-    let array = Array.from(chosen);
-    let idName = array.pop()
-    idName.innerHTML = "O"
- }
 
 
 
-
-let choice;
-
-document.querySelector('.choiceX').addEventListener('click', function() {
-     choice = 'x';
-     if (choice === 'x') {
-        document.querySelector('.grid_container').addEventListener('click', chooseX);
-       }
-       removeListener(chooseO)
-})
-
-document.querySelector('.choiceO').addEventListener('click', function() {
-     choice = 'o';
-     if (choice === 'o') {
-        document.querySelector('.grid_container').addEventListener('click', chooseO);
-       }
-       removeListener(chooseX)
-})
-
-
-
-
- removeListener = (funct) => {
-     document.querySelector('.grid_container').removeEventListener('click', funct)
- }
 
  
 
